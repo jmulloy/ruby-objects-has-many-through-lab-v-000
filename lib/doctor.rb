97c7296531aad62bfe65aa1e5ff1769863673af4
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-11 13:09:46 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,7 +22,7 @@ attr_accessor :name, :patient
    Appointment.all.select {|appointment| appointment.doctor == self}
  end
 
-def new_appointment(date, patient)
+ def new_appointment(date, patient)
     Appointment.new(date, self, patient)
   end
 end
